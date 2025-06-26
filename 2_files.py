251fd670a867d583420cd268823084c7b2ecf73a
Ivan Sokolov <volokos@bk.ru>
2025-06-26 19:55:42 +0300

    Готовое ДЗ

--- 2_files.py
+++ 2_files.py
@@ -8,15 +8,22 @@
 2. Прочитайте содержимое файла в перменную, подсчитайте длинну получившейся строки
 3. Подсчитайте количество слов в тексте
 4. Замените точки в тексте на восклицательные знаки
-5. Сохраните результат в файл referat2.txt
-"""
+5. Сохраните результат в файл referat2.txt"""
 
-def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
-
+from collections import Counter
+    
+def main():   
+	with open('referat.txt', 'r', encoding='utf-8') as file1:
+		fread=file1.read()
+		print(fread)
+		dlina=len(fread)
+		print(dlina)
+		num_words=len(fread.split())
+		print(num_words)
+		new_fread = fread.replace('.', '!')
+		print(new_fread)
+	with open('referat2.txt', 'w', encoding='utf-8') as file2:	
+		file2.write(new_fread)
+		
 if __name__ == "__main__":
     main()
